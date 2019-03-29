c17704aa7777db52915aa6dfcdadbf0c69cb9305
USER <user@owner-2.local>
2019-03-29 15:04:28 -0400

    some work done

---
+++ phase2/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+// Connect to mongodb
+
+mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true});
+
+mongoose.connection.once('open',()=>{
+	console.log('connection has made been made!')
+}).on('error',()=> {
+	console.log('error!')
+})
