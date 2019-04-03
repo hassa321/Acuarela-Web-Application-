e6ccf21d3cacfd5000c08975b930d1fc4e45a993
USER <user@owner-2.local>
2019-04-03 19:02:01 -0400

    post.ad

--- public/post.js
+++ public/post.js
@@ -1,11 +1,13 @@
 
 const log = console.log;
 
+const postForm = document.querySelector('.form-post');
+postForm.addEventListener('submit', addPostDb);
 
 
-
-function addPostDb() {
+function addPostDb(e) {
 	// get values of post ad 
+	e.preventDefault()
 	const cat = document.querySelector('#category')
 	const category = cat.options[cat.selectedIndex].text;
 	const ad = document.querySelector('#type')
@@ -14,19 +16,23 @@ function addPostDb() {
 	const desc = document.querySelector('#description').value
 	const loc = document.querySelector('#location')
 	const location = loc.options[loc.selectedIndex].text;
-	const price = document.querySelector('#price').value
+	const price = document.querySelector('#pric').value
 
 
 	//error checking, making sure all fields are filled in correctly
-	if((title.length<1) || (desc.length<1) || (type==="Choose the Ad type") || (category==="Choose a subject category") ||(location === "Location") ||(price === "")){
-		alert('Please fill out all fields!')
+	//if((title.length<1)){// || (desc.length<1) || (type==="Choose the Ad type") || (category==="Choose a subject category") ||(location === "Location") ||(price === "")){
+		//alert('Please fill out all fields!')
 	
 
-	}else{
+	//}else{
 		//all fields are correctly filled out
 		// need to add ad to database
 		// then send user back to homepage
 		var data;
+		log(title)
+		log(price)
+		log(type)
+		log(location)
 
 	$.ajax({ 
 		method: "post", 
@@ -54,8 +60,6 @@ function addPostDb() {
 
 
 }
-
-    }
 
 
 	
