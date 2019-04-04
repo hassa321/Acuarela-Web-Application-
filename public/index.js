f6696a23a8dd1701ce900a0d776189e9253362df
USER <user@owner-2.local>
2019-04-04 00:01:51 -0400

    heroku app test17

--- public/index.js
+++ public/index.js
@@ -17,89 +17,79 @@ document.addEventListener("DOMContentLoaded", function() {
   $.ajax({ 
     method: "get", 
     url: "/ads",
-    
-  }).done(function(data){
+    success: function(data){
    console.log(data)
-
-   //server call brings all ads in JSON file
-   // check if JSON is empty, means there are no ads
-
-
-    if(data.length<1){
-      console.log("EMPTY NO ADS")
-    }else{
-      console.log(`There ARE ${data.length} number of ads`)
     }
+  })
+})
+
+//    //server call brings all ads in JSON file
+//    // check if JSON is empty, means there are no ads
 
 
-    console.log(`array of ads${data}`)  
-    const dom = document.getElementById("ad-section")
-    var posting;
-    var thumbnail;
-    var img;
-    var caption;
-    var h4;
-    var h4text;
-    var price;
-    var priceNumber;
+//     if(data.length<1){
+//       console.log("EMPTY NO ADS")
+//     }else{
+//       console.log(`There ARE ${data.length} number of ads`)
+//     }
 
 
-    for (let i = 0;i<data.length;i++){
-    //create ALL dom Elements first
-     posting = document.createElement('div')
-    posting.classList.add('posting');
-     thumbnail = document.createElement('div')
-    thumbnail.classList.add('thumbnail');
-     img = document.createElement('img')
-     if(data[i].type==="Textbook"){img.src="https://image.slidesharecdn.com/introductiontoalgorithms3rdedition-140128021114-phpapp02/95/introduction-to-algorithms-3rd-edition-1-638.jpg?cb=1390876516"}
-      if (data[i].type==="iClicker"){img.src="../public/images/iClickers.jpg"}
-      if(data[i].type==="Electronics"){img.src="../public/images/Electronics.jpg"}
-      if(data[i].type==="Course Notes"){img.src="../public/images/notes.jpg"}
-        if(data[i].type==="Calculator"){img.src="../public/images/Calculators.jpg"}
-          if(data[i].type==="Others"){img.src="../public/images/others.png"}
-     caption = document.createElement('div')
-    caption.classList.add('caption')
-     h4 = document.createElement('h4')
-     h4text = document.createTextNode(data[i].title)
-    h4.appendChild(h4text)
-     price = document.createElement('p')
-     priceNumber = document.createTextNode(`List Price: ${data[i].price}`)
-    price.appendChild(priceNumber)
+//     console.log(`array of ads${data}`)  
+//     const dom = document.getElementById("ad-section")
+//     var posting;
+//     var thumbnail;
+//     var img;
+//     var caption;
+//     var h4;
+//     var h4text;
+//     var price;
+//     var priceNumber;
+
+
+//     for (let i = 0;i<data.length;i++){
+//     //create ALL dom Elements first
+//      posting = document.createElement('div')
+//     posting.classList.add('posting');
+//      thumbnail = document.createElement('div')
+//     thumbnail.classList.add('thumbnail');
+//      img = document.createElement('img')
+//      if(data[i].type==="Textbook"){img.src="https://image.slidesharecdn.com/introductiontoalgorithms3rdedition-140128021114-phpapp02/95/introduction-to-algorithms-3rd-edition-1-638.jpg?cb=1390876516"}
+//       if (data[i].type==="iClicker"){img.src="../public/images/iClickers.jpg"}
+//       if(data[i].type==="Electronics"){img.src="../public/images/Electronics.jpg"}
+//       if(data[i].type==="Course Notes"){img.src="../public/images/notes.jpg"}
+//         if(data[i].type==="Calculator"){img.src="../public/images/Calculators.jpg"}
+//           if(data[i].type==="Others"){img.src="../public/images/others.png"}
+//      caption = document.createElement('div')
+//     caption.classList.add('caption')
+//      h4 = document.createElement('h4')
+//      h4text = document.createTextNode(data[i].title)
+//     h4.appendChild(h4text)
+//      price = document.createElement('p')
+//      priceNumber = document.createTextNode(`List Price: ${data[i].price}`)
+//     price.appendChild(priceNumber)
     
     
     
     
-    posting.appendChild(thumbnail)
-    thumbnail.appendChild(img)
-    thumbnail.appendChild(caption)
-    caption.appendChild(h4)
-    caption.appendChild(price)
-    console.log(posting)
-    console.log(dom)
+//     posting.appendChild(thumbnail)
+//     thumbnail.appendChild(img)
+//     thumbnail.appendChild(caption)
+//     caption.appendChild(h4)
+//     caption.appendChild(price)
+//     console.log(posting)
+//     console.log(dom)
     
     
-  }
+//   }
     
-    
+//    console.log("ERROR HEr1!")
+//   });
 
 
 
+// console.log('LOADED!!')
 
 
-
-
-
-
-  }).fail(function(err){
-    
-    console.log("ERROR HEr1!")
-  });
-
-
-
-console.log('LOADED!!')
-
-
-});
+// });
 
 
