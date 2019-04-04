4ef93ce97e50b2b87c05c56223b6bfb6c675f7fb
USER <user@utmuser150-195.wireless.utoronto.ca>
2019-04-04 15:28:06 -0400

    heroku app test17

--- public/index.js
+++ public/index.js
@@ -15,8 +15,8 @@ document.addEventListener("DOMContentLoaded", function() {
     url: "/ads",
     }).done(function(data){
    console.log(data)
-      const d =document.getElementById("abc")
-      d.innerHTML='abc'
+      const d =document.getElementById("ad-section")
+      d.appendChild(document.createElement('div'))
    //server call brings all ads in JSON file
    // check if JSON is empty, means there are no ads
     console.log(`Number of Ads:${data.length}`)
@@ -31,54 +31,6 @@ document.addEventListener("DOMContentLoaded", function() {
 // do everything here.
     
   console.log('LOADED!!')
-  const dom = document.getElementById("ad-section")
-  var varray
-  var posting;
-  var thumbnail;
-  var img;
-  var caption;
-  var h4;
-  var h4text;
-  var price;
-  var priceNumber;
-
-
-  for (let i = 0;i<data1.length;i++){
-  //create ALL dom Elements first
-   posting = document.createElement('div')
-  posting.classList.add('posting');
-   thumbnail = document.createElement('div')
-  thumbnail.classList.add('thumbnail');
-   img = document.createElement('img')
-   if(data1[i].type==="Textbook"){img.src="https://image.slidesharecdn.com/introductiontoalgorithms3rdedition-140128021114-phpapp02/95/introduction-to-algorithms-3rd-edition-1-638.jpg?cb=1390876516"}
-    if (data1[i].type==="iClicker"){img.src="../public/images/iClickers.jpg"}
-    if(data1[i].type==="Electronics"){img.src="../public/images/Electronics.jpg"}
-    if(data1[i].type==="Course Notes"){img.src="../public/images/notes.jpg"}
-      if(data1[i].type==="Calculator"){img.src="../public/images/Calculators.jpg"}
-        if(data1[i].type==="Others"){img.src="../public/images/others.png"}
-   caption = document.createElement('div')
-  caption.classList.add('caption')
-   h4 = document.createElement('h4')
-   h4text = document.createTextNode(data1[i].title)
-  h4.appendChild(h4text)
-   price = document.createElement('p')
-   priceNumber = document.createTextNode(`List Price: ${data1[i].price}`)
-  price.appendChild(priceNumber)
-  
-  
-  
-  
-  posting.appendChild(thumbnail)
-  thumbnail.appendChild(img)
-  thumbnail.appendChild(caption)
-  caption.appendChild(h4)
-  caption.appendChild(price)
-  console.log('appending here')
-  dom.append(posting)
-  console.log(posting)
-  console.log(dom)
-
-}
-  });
+});
 
 
