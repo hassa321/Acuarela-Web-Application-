a7850174b072eb7b89a3d61e41abf5c55e7a95de
USER <user@owner-2.local>
2020-07-14 03:25:47 -0400

    N/A

--- public/index.js
+++ public/index.js
@@ -89,7 +89,7 @@ function loadAds (data,category,condition,location){
         
     ), 
     $("<div>", {class: "inner"}).append(
-        $("h4>a",{class: "product_title",href: "https://google.ca"}).text(
+        $("<a>",{class: "product_title",href: "https://google.ca"}).text(
             title
         )
     ,$("<h3>",{class: ""}).text(
