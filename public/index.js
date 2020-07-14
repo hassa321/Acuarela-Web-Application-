2eccba64293472da558a3896dd2ac71b66b09be8
USER <user@owner-2.local>
2020-07-14 00:25:46 -0400

    N/A

--- public/index.js
+++ public/index.js
@@ -1,12 +1,3 @@
-
-
-// const classes = document.querySelectorAll('.classButton');
-// const postings = document.querySelectorAll('.posting');
-
-//window.open(window.open('../views/index.html','_self'))
-
-
-
 document.addEventListener("DOMContentLoaded", function() {
    // server call 
   // server call will bring JSON file of ad elemets
@@ -19,39 +10,11 @@ document.addEventListener("DOMContentLoaded", function() {
     var type;
     var price;
     var priceNumber;
-    const d =document.getElementById("abc")
     $.ajax({ 
     method: "get", 
     url: "/ads",
     }).done(function(data){
-      for(var i = 0; i<data.length;i++){
-
-        price = `$${data[i].price}`
-        title = data[i].title
-        var type = data[i].type
-        if(data[i].type==="Textbook"){img="https://image.slidesharecdn.com/introductiontoalgorithms3rdedition-140128021114-phpapp02/95/introduction-to-algorithms-3rd-edition-1-638.jpg?cb=1390876516"}
-        if (data[i].type==="iClicker"){img="../public/images/iClickers.jpg"}
-        if(data[i].type==="Electronics"){img="../public/images/Electronics.jpg"}
-        if(data[i].type==="Course Notes"){img="../public/images/notes.jpg"}
-          if(data[i].type==="Calculator"){img="../public/images/Calculators.jpg"}
-            if(data[i].type==="Others"){img="../public/images/others.png"}
-
-      $("<div>", {class: "posting"}).append(
-        $("<div>", {class: "thumbnail"}).append(
-            $("<img>",{src:img, class: "circle"}),
-            $("<div", {class:"caption"}).append(
-            )
-            
-        ), 
-        $("<div>", {class: "inner"}).append(
-            $("<h4>",{class: "product_title"}).text(
-                title
-            )
-        ,$("<h4>",{class: ""}).text(
-          price
-      ))
-    ).appendTo(d)
-            }
+      loadAds(data,0,0,0,0)
     }).fail(function(err){
     
     console.log("ERROR HEr1!")
@@ -75,7 +38,8 @@ $(".filter-btn").click(function(){
 
 
   console.log("category: "+category+" type: "+type+" condition: "+condition + " location: "+location);
-
+  
+  loadAds(category,type,condition,location)
 
 
 
@@ -93,7 +57,37 @@ $(".clear-btn").click(function(){
 })
 
 
+function loadAds (data,category,type,condition,location){
+  for(var i = 0; i<data.length;i++){
 
+    price = `$${data[i].price}`
+    title = data[i].title
+    var type = data[i].type
+    if(data[i].type==="Textbook"){img="https://image.slidesharecdn.com/introductiontoalgorithms3rdedition-140128021114-phpapp02/95/introduction-to-algorithms-3rd-edition-1-638.jpg?cb=1390876516"}
+    if (data[i].type==="iClicker"){img="../public/images/iClickers.jpg"}
+    if(data[i].type==="Electronics"){img="../public/images/Electronics.jpg"}
+    if(data[i].type==="Course Notes"){img="../public/images/notes.jpg"}
+      if(data[i].type==="Calculator"){img="../public/images/Calculators.jpg"}
+        if(data[i].type==="Others"){img="../public/images/others.png"}
+
+  $("<div>", {class: "posting"}).append(
+    $("<div>", {class: "thumbnail"}).append(
+        $("<img>",{src:img, class: "circle"}),
+        $("<div", {class:"caption"}).append(
+        )
+        
+    ), 
+    $("<div>", {class: "inner"}).append(
+        $("<h4>",{class: "product_title"}).text(
+            title
+        )
+    ,$("<h4>",{class: ""}).text(
+      price
+  ))
+).appendTo($("#abc"))
+        }
+
+}
 
   
   });
